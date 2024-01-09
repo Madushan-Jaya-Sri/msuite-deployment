0ceec164674a22a15028e8ea1a3d9cb0ed6c453b
madushan <madushanjayasri100@gmail.com>
2024-01-09 21:33:52 +0530

    updated views

--- m_suite/views.py
+++ m_suite/views.py
@@ -399,6 +399,7 @@ def proceed_yt_url(request):
 
     # Initialize the Chrome WebDriver with the configured options
     driver = webdriver.Chrome(options=chrome_options)
+    #driver.maximize_window()
 
     yt_url = "" 
     
@@ -422,13 +423,15 @@ def proceed_yt_url(request):
             
             details = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, ' //*[@id="content"]')))
             details.click()
-        
+            
+            print(details.text)
+            
             time.sleep(3)
             
-            #about = driver.find_elements(By.XPATH, '//*[@id="contents"]')
-            about = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="contents"]')))
-            driver.save_screenshot('screenshot.png') 
-            print(about.text)
+            about = driver.find_elements(By.XPATH, '//*[@id="contents"]')
+            #about = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="contents"]')))
+            driver.save_screenshot('screenshot2.png') 
+            
             text = about[-1].text
             print(text)
             #logging.info(f'Text : {text}')
